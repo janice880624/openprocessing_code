fbeb32c0ad400321ca614007c12624bb84f5e52e
janicekuo <janice880624@gmail.com>
2022-05-17 15:20:39 +0800

    0517

--- main/sketch.js
+++ main/sketch.js
@@ -1,7 +1,11 @@
-let nosex=0, nosey=0;
-let nosenx=0;
-let i=0;
-let video;
+var nosex=0, nosey=0;
+var nosenx=0;
+var i=0;
+var video;
+var b;
+var bt1;
+var bt2;
+var j=30;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -13,6 +17,10 @@ function setup() {
 
   pose = ml5.poseNet(video);
   pose.on('pose', getPoses);
+
+  b = new Ball(windowWidth/2, windowHeight/2, 50);
+  bt1 = new Bat(200, windowHeight-i);
+  bt2 = new Bat(200, i);
 }
 
 function draw() {
@@ -34,7 +42,15 @@ function draw() {
 
   updatePixels();
 
-  fill(255, 0, 0);
+  b.move();
+  b.checkEdge();
+
+  fill(255, 255, 0);
+  b.show();
+
+  bt1.show();
+  bt2.show();
+
   if (nosex > windowWidth/2){
     i = nosex - windowWidth/2;
     nosenx = nosex - 2*i
@@ -42,12 +58,75 @@ function draw() {
     i = windowWidth/2 - nosex;
     nosenx = nosex + 2*i
   }
-  ellipse(nosenx, nosey, 50)
+
+  if (b.y >= windowHeight-j || b.y <= j) {
+    if (b.x <= nosex + (this.width / 2) && b.x >= nosex - (this.width / 2)) {
+      b.bounce();
+    }
+  }
 }
 
 function getPoses(poses) {
   if(poses.length > 0){
     nosex = poses[0].pose.keypoints[0].position.x;
     nosey = poses[0].pose.keypoints[0].position.y;
+  }
+}
+
+class Ball {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.xSpeed = 10;
+    this.ySpeed = 15;
+  }
+
+  show() {
+    ellipse(this.x, this.y, this.r);
+  }
+
+  move() {
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }
+
+  checkEdge() {
+    if (this.x >= width || this.x <= 0) {
+      this.xSpeed *= -1;
+    }
+
+    if (this.y >= height || this.y <= 0) {
+      this.ySpeed *= -1;
+    }
+  }
+
+  bounce() {
+    this.ySpeed *= -1;
+  }
+}
+
+class Bat {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 200;
+    this.height = 40;
+  }
+
+  show() {
+
+    fill(255, 0, 0);
+    rectMode(CENTER);
+
+    if (nosex > windowWidth/2){
+      i = nosex - windowWidth/2;
+      nosenx = nosex - 2*i
+    } else {
+      i = windowWidth/2 - nosex;
+      nosenx = nosex + 2*i
+    }
+
+    rect(nosenx, this.y, this.width, this.height);
   }
 }
